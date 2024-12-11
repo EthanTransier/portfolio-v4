aa5d9c30271dd487756df258c488809d2eff2d99
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2024-12-11 09:57:46 -0700

    Home screen setup and about page transition

---
+++ src/app/about/page.js
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <main className="about-main home-main">
+        <div className="about-title-container">
+            <h1>about</h1>
+        </div>
+        
+      </main>
+    </div>
+  );
+}
